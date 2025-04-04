e8f17a1deadcf23c6cc0fa74e17351bd98fc1126
Mohsina <mohsina734parveen@gmail.com>
2025-04-04 23:01:44 +0530

    Fix import path for MainPage component to match file naming convention

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from './Components/Navbar';
 import Home from './Components/Home';
 import About from './Components/About';
 import Contact from './Components/Contact';
-import MainPage from './Components/MainPage';
+import MainPage from './Components/Mainpage';
 
 const ScrollWrapper = () => {
   const homeRef = useRef(null);
