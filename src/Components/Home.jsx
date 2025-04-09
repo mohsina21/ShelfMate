7e4581f42619929197ff302fa334ce7f6f0c43eb
Mohsina <mohsina734parveen@gmail.com>
2025-04-09 22:23:01 +0530

    Refactor Home component to enhance navigation and improve user experience

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { LampContainer } from "./LampDemo";  
-import { TypewriterEffectSmooth } from './TypeWriterEffect';
+import { TypewriterEffectSmooth } from './TypewriterEffect';
 
 const Home = () => {
   const navigate = useNavigate();
